6829aebf1c6dc4f231f5495c9d8573a8bca53b4c
Oleksandr Zhurba <a.zhurbinho@gmail.com>
2024-10-10 01:41:30 +0200

    feat: changed nav bar to antd <Menu> changed <Login> page to antd design

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,65 @@ import Register from "./pages/register";
 import Login from "./pages/login";
 import Profile from "./pages/profile";
 import ProtectedRoute from "./components/protectedRoute";
+import { Flex, Menu } from "antd";
+import { useEffect, useState } from "react";
+import {
+  HomeOutlined,
+  LoginOutlined,
+  UserAddOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import Home from "./pages/home";
 
 function App() {
+  const [current, setCurrent] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const token = localStorage.getItem("token");
+  function handleIsLoggedIn() {
+    return token ? setIsLoggedIn(true) : setIsLoggedIn(false);
+  }
+  const onClick = (event) => {
+    setCurrent(event.key);
+  };
+  const items = [
+    {
+      key: "home",
+      label: <Link to="/">Home</Link>,
+      icon: <HomeOutlined />,
+    },
+    {
+      key: "login",
+      label: <Link to="/login">Login</Link>,
+      icon: <LoginOutlined />,
+    },
+    {
+      key: "register",
+      label: <Link to="/register">Register</Link>,
+      icon: <UserAddOutlined />,
+    },
+    {
+      key: "profile",
+      label: <Link to="/profile">Profile</Link>,
+      icon: <UserOutlined />,
+      disabled: isLoggedIn,
+    },
+  ];
+  useEffect(() => {
+    handleIsLoggedIn();
+  }, [isLoggedIn, handleIsLoggedIn]);
   return (
-    <>
-      <nav>
+    <Flex gap="middle" vertical justify="start" align="center">
+      {/* <nav>
         <Link to="/">Home</Link>
         <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
         <Link to="/profile">Profile</Link>
-      </nav>
+      </nav> */}
+      <Menu
+        onClick={onClick}
+        selectedKeys={{ current }}
+        mode="horizontal"
+        items={items}
+      />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
@@ -27,11 +76,8 @@ function App() {
           }
         />
       </Routes>
-    </>
+    </Flex>
   );
-}
-function Home() {
-  return <div>home</div>;
 }
 
 export default App;
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -2,12 +2,14 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { login, resetState } from "../../redux/slices/authSlice";
+import { Flex, Button, Form, Input, Space } from "antd";
 
 export default function Login() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const [formData, setFormData] = useState({ email: "", password: "" });
-  const { email, password } = formData;
+  const [form] = Form.useForm();
+  // const { email, password } = formData;
 
   const { isLoading, isError, isSuccess, message } = useSelector(
     (state) => state.auth
@@ -15,46 +17,75 @@ export default function Login() {
 
   function handleChange(event) {
     const { name, value } = event.target;
-    setFormData({
-      ...formData,
+    setFormData((prev) => ({
+      ...prev,
       [name]: value,
-    });
+    }));
   }
-  function handleSubmit(event) {
-    event.preventDefault();
-    dispatch(login({ email, password }));
+  function handleSubmit() {
+    dispatch(login({ email: formData.email, password: formData.password }));
+  }
+  function handleReset() {
+    form.resetFields();
   }
 
   useEffect(() => {
     if (isSuccess) {
       navigate("/profile");
     }
+
     return () => {
       dispatch(resetState());
     };
   }, [isSuccess, navigate, dispatch]);
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          value={email}
-          type="email"
+    <Flex justify="center" align="center">
+      <Form
+        form={form}
+        name="login"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        style={{ maxWidth: 500 }}
+        autoComplete="off"
+        onFinish={handleSubmit}
+      >
+        <Form.Item
+          label="email"
           name="email"
-          placeholder="Email"
-          onChange={handleChange}
-        />
-        <input
-          value={password}
-          type="password"
+          rules={[{ required: true, message: "Please fill email!" }]}
+        >
+          <Input name="email" value={formData.email} onChange={handleChange} />
+        </Form.Item>
+        <Form.Item
+          label="password"
           name="password"
-          placeholder="Password"
-          onChange={handleChange}
-        />
-        <button type="submit">Login</button>
-      </form>
+          rules={[
+            {
+              required: true,
+              message: "Please fill the password",
+            },
+          ]}
+        >
+          <Input
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </Form.Item>
+        <Form.Item wrapperCol={{ span: 8, offset: 8 }}>
+          <Space>
+            <Button type="primary" htmlType="submit">
+              Login
+            </Button>
+            <Button onClick={handleReset} htmlType="button">
+              Reset
+            </Button>
+          </Space>
+        </Form.Item>
+      </Form>
       {isError && <p>{message}</p>}
       {isSuccess && <p>Login successful</p>}
-    </div>
+    </Flex>
   );
 }
